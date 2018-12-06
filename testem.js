ddb6bd18ee72a2e859fc132c110d6db5b5641d06
Kevin Glinski <kevin.glinski@inin.com>
2018-12-06 11:22:08 -0500

    testem use ember chromium

--- testem.js
+++ testem.js
@@ -1,12 +1,3 @@
 /*jshint node:true*/
-module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "Chrome"
-  ],
-  "launch_in_dev": [
-    "Chrome"
-  ]
-};
+module.exports = require('ember-chromium').getTestemConfig();
+
