7d14885342890600a0cacf04ac56c30bfd2d579b
Kevin Glinski <kevin.glinski@inin.com>
2018-12-06 11:52:04 -0500

    chrome args

--- testem.js
+++ testem.js
@@ -3,12 +3,7 @@ const chromium = require('ember-chromium');
 // any chromium flags you want
 const chromiumArgs = [
   '--disable-gpu',
-  '--no-sandbox',
-  '--disable-gesture-requirement-for-media-playback',
-  '--allow-file-access',
-  '--use-fake-device-for-media-stream',
-  '--use-fake-ui-for-media-stream'
-];
+  '--no-sandbox'];
 
 const config = chromium.getTestemConfig(chromiumArgs);
 module.export = config;
