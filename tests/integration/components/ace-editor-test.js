a78a6d971cd46675f4ca392e735621173209c200
Kevin Glinski <kevin.glinski@inin.com>
2018-12-06 10:18:39 -0500

    upgrade ember version, fix lint errors

--- tests/integration/components/ace-editor-test.js
+++ tests/integration/components/ace-editor-test.js
@@ -1,17 +1,17 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('ace-editor', 'Integration | Component | ace editor', {
+moduleForComponent("ace-editor", "Integration | Component | ace editor", {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test("it renders", function(assert) {
+  // Set any properties with this.set("myProperty", "value");
+  // Handle any actions with this.on("myAction", function(val) { ... });
 
   this.render(hbs`{{ace-editor}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), "");
 
 
 });
