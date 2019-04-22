baf6502a68b89f3fce3477d0d5a386c2ddfb5ce6
Kevin Glinski <kevin.glinski@inin.com>
2019-04-22 13:19:30 -0400

    fixing auth header not showing in curl

--- app/components/request-share.js
+++ app/components/request-share.js
@@ -6,11 +6,14 @@ export default Ember.Component.extend({
     shareTypes : Ember.String.w('Link cURL'),
     selectedShareType: 'Link',
     includeAuthHeader: false,
-
+    init(){
+        this._super(...arguments);
+        this.get("includeAuthHeader");
+    },
     sharableLink: computed('shareService.sharableLink', function() {
         return this.get('shareService').get('sharableLink');
     }),
-    sharableCurl: computed('shareService.sharableCurl,includeAuthHeader', function() {
+    sharableCurl: computed('shareService.sharableCurl','includeAuthHeader', function() {
         if(this.get("includeAuthHeader")){
             return this.get('shareService').get('sharableCurlWithAuth');
         }
